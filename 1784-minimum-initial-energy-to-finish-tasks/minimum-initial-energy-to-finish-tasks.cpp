895a623ddaaa46afddb591ab09eb5a525543bbff
241b022-hue <241b022@juetguna.in>
2026-05-12 19:43:20 +0530

    Time: 35 ms (71.84%) | Memory: 97.3 MB (63.11%) - LeetSync

---
+++ 1784-minimum-initial-energy-to-finish-tasks/minimum-initial-energy-to-finish-tasks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), [](vector<int>& a, vector<int>& b) {
+            return (a[1] - a[0]) > (b[1] - b[0]);
+        });
+
+        int energy = 0;
+        int curr = 0;
+
+        for (auto& t : tasks) {
+            int actual = t[0];
+            int minimum = t[1];
+
+            if (curr < minimum) {
+                energy += (minimum - curr);
+                curr = minimum;
+            }
+
+            curr -= actual;
+        }
+
+        return energy;
+    }
+};
